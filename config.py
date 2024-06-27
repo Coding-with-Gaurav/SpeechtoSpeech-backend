7a7e00b6d9834b8f3d4d90798e6918c10af2032c
Gaurav <codewithgaurav.dsvv@gmail.com>
2024-06-27 12:08:07 +0530

    some modifications for debug

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 class Config:
     SECRET_KEY = 'speechTospeech'
-    # Add other common configurations
 
 class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
-    # Add production-specific configurations
 
 class DevelopmentConfig(Config):
     DEBUG = True
     TESTING = True
-    # Add development-specific configurations
+
+
